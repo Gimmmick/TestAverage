ac21d6f3c750659cf961fdae67ca023ba78423dd
Gimmmick <vlada310999@mail.ru>
2021-04-06 13:18:30 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+The program calculates the average test score using user's input scores for 5
+tests. 
+
+@author Vlada Foulke
+@version 10/4/2020
+*******************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double Test1, Test2, Test3, Test4, Test5;
+    
+    cout << "Enter your score for each of the tests" << endl;
+    cin >> Test1 >> Test2 >> Test3 >> Test4 >> Test5;
+
+    double averageScore = (Test1 + Test2 + Test3 + Test4 + Test5) / 5;
+
+    cout << "Your average score is " << fixed << setprecision(1) << averageScore;
+    
+    return 0;
+}
+
